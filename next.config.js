b38c21a94df5fcc0d3094785886f38efb1df339c
omondi <onyangodonomoni@gmail.com>
2025-03-13 15:38:13 +0000

    Update Next.js configuration: disable image optimization, specify image formats and sizes, add allowed domains, and set output to standalone.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: false,
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ['mocky.co.ke'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -16,9 +20,8 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: [], // Add any external domains if needed
   },
-  // Add any other Next.js configuration options here
+  output: 'standalone'
 };
 
 module.exports = nextConfig; 
